46ad383704cc2a4ae6f7bfbc7dc7b319f28997a0
SitharaFathima <sitharafathima999@gmail.com>
2022-11-14 21:02:29 +0530

    widgets

--- users/forms.py
+++ users/forms.py
@@ -6,3 +6,9 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ["first_name", "last_name", "email", "username", "password"]
+        widgets = {
+            "password" : forms.widgets.PasswordInput(attrs={
+                "class": "one",
+                "Placeholder":"Enter strong password"
+            })
+        }
